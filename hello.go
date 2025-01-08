2450ea3c0dda241f04c8a301f913a8c872f91356
Awalhossain <waqin21@gmail.com>
2025-01-08 13:25:00 +0600

    add hello.go with a basic switch case example

---
+++ hello.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 020
+
+	switch a {
+	case 10:
+		fmt.Println("10")
+	case 20:
+		fmt.Println("20")
+	default:
+		fmt.Println("default")
+	}
+
+	// fmt.Println("trying", s)
+}
+
+// call the function the main function
